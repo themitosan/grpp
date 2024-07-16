ba9fc3bf223b4a506c525d3bf6a0adf26ddf3b40
themitosan <juliodogers@gmail.com>
2024-07-16 01:29:07 -0300

    +

--- src/import.js
+++ src/import.js
@@ -34,12 +34,15 @@ const repoName = getRepoName(process.argv[2]);
     * @returns repo name
 */
 function getRepoName(str){
-    var res = str.split('/');
+    var res = [];
+    if (str !== void 0){
+        res = str.split('/');
+    }
     return res[res.length - 1];
 }
 
 /**
-    * Start main process 
+    * Start main process
 */
 function startCheck(){
 
@@ -81,66 +84,73 @@ function startCheck(){
 
     };
 
-    // Spawn git clone command
-    console.info(`INFO - Importing \x1b[33m${repoName}\x1b[0m - Please wait...`);
-    const gitClone = module_child_process.spawn('git', ['clone', '--progress', '--bare', '--mirror', process.argv[2], `${cSettings.clonePath}/${repoName}`]);
+    // Check if can clone
+    if (process.argv[2] !== void 0){
 
-    // Capture git data
-    gitClone.stderr.on('data', function(data){
-        msgData = `${msgData}${data}\n`;
-        console.info(data.toString());
-    });
-    gitClone.stdout.on('data', function(data){
-        msgData = `${msgData}${data}\n`;
-        console.info(data.toString());
-    });
-    gitClone.stdin.on('data', function(data){
-        msgData = `${msgData}${data}\n`;
-        console.info(data.toString());
-    });
+        // Spawn git clone command
+        console.info(`INFO - Importing \x1b[33m${repoName}\x1b[0m - Please wait...`);
+        const gitClone = module_child_process.spawn('git', ['clone', '--progress', '--bare', '--mirror', process.argv[2], `${cSettings.clonePath}/${repoName}`]);
 
-    // Take action after process closing
-    gitClone.on('exit', function(code){
-
-        // Output exit code, cut final line break from git output and log it's data
-        console.info(`INFO - Git closed with code ${code}.`);
-        msgData = msgData.slice(0, msgData.length - 1);
-
-        // Process error list
-        Object.keys(errorDatabase).forEach(function(cError){
-            errorDatabase[cError].str.forEach(function(cStr){
-                if (msgData.indexOf(cStr) !== -1){
-                    errorList.push(cError);
-                }
-            });
+        // Capture git data
+        gitClone.stderr.on('data', function(data){
+            msgData = `${msgData}${data}\n`;
+            console.info(data.toString());
+        });
+        gitClone.stdout.on('data', function(data){
+            msgData = `${msgData}${data}\n`;
+            console.info(data.toString());
+        });
+        gitClone.stdin.on('data', function(data){
+            msgData = `${msgData}${data}\n`;
+            console.info(data.toString());
         });
 
-        // Check if we had any errors
-        if (errorList.length === 0){
-            
-            // Log process
-            console.info('INFO - No errors were detected during clone process!');
-            console.info('INFO - Now... Let\'s make it a safe directory.');
+        // Take action after process closing
+        gitClone.on('exit', function(code){
 
-            // Mark current repo as a safe dir
-            const makeTrust = module_child_process.spawn('git', ['config', '--global', '--add', 'safe.directory', `${process.cwd()}/${cSettings.clonePath}/${repoName}`]);
-            makeTrust.on('exit', function(data){
-                console.info(`INFO - Set ${repoName} dir as a safe dir closed with code ${data}!`);
-                console.info('\n=== Process complete! ===\n');
+            // Output exit code, cut final line break from git output and log it's data
+            console.info(`INFO - Git closed with code ${code}.`);
+            msgData = msgData.slice(0, msgData.length - 1);
+
+            // Process error list
+            Object.keys(errorDatabase).forEach(function(cError){
+                errorDatabase[cError].str.forEach(function(cStr){
+                    if (msgData.indexOf(cStr) !== -1){
+                        errorList.push(cError);
+                    }
+                });
             });
 
-        } else {
+            // Check if we had any errors
+            if (errorList.length === 0){
 
-            // Create error message string and output data
-            var errStr = '';
-            errorList.forEach(function(cError){
-                errStr = `${errStr}${errorDatabase[cError].message}\n`;
-            });
-            console.error(`\x1b[31mERROR - Unable to clone ${repoName}!\n\x1b[0mReason: ${errStr.slice(0, errStr.length - 1)}\n`);
+                // Log process
+                console.info('INFO - No errors were detected during clone process!');
+                console.info('INFO - Now... Let\'s make it a safe directory.');
 
-        }
+                // Mark current repo as a safe dir
+                const makeTrust = module_child_process.spawn('git', ['config', '--global', '--add', 'safe.directory', `${process.cwd()}/${cSettings.clonePath}/${repoName}`]);
+                makeTrust.on('exit', function(data){
+                    console.info(`INFO - Set ${repoName} dir as a safe dir closed with code ${data}!`);
+                    console.info('\n=== Process complete! ===\n');
+                });
 
-    });
+            } else {
+
+                // Create error message string and output data
+                var errStr = '';
+                errorList.forEach(function(cError){
+                    errStr = `${errStr}${errorDatabase[cError].message}\n`;
+                });
+                console.error(`\x1b[31mERROR - Unable to clone ${repoName}!\n\x1b[0mReason: ${errStr.slice(0, errStr.length - 1)}\n`);
+
+            }
+
+        });
+
+    } else {
+        console.error('\x1b[31mERROR - You must provide a git url!\n');
+    }
 
 }
 
