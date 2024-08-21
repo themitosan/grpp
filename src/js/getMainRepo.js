cba4231261288d0530dba014a60a02a0ed0a9135
themitosan <juliodogers@gmail.com>
2024-08-21 15:51:52 -0300

    +

--- src/js/getMainRepo.js
+++ src/js/getMainRepo.js
@@ -14,7 +14,7 @@ const module_fs = require('fs');
 */
 
 // Default settings
-var cSettings = structuredClone(JSON.parse(module_fs.readFileSync('src/json/settings_template.json', 'utf-8')));
+var cSettings = structuredClone(JSON.parse(module_fs.readFileSync('settings.json', 'utf-8')));
 
 /*
     Functions
@@ -24,13 +24,6 @@ var cSettings = structuredClone(JSON.parse(module_fs.readFileSync('src/json/sett
     * Main function
 */
 function getMainRepo(){
-
-    // Check if settings file exists
-    if (module_fs.existsSync('settings.json') === !1){
-        module_fs.writeFileSync('settings.json', JSON.stringify(cSettings), 'utf-8');
-    } else {
-        cSettings = structuredClone(JSON.parse(module_fs.readFileSync('settings.json', 'utf-8')));
-    }
 
     // Return clone path
     console.info(cSettings.clonePath);
