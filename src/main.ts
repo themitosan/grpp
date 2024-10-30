1d7dc28b3f81305e937230bd3048049ead584832
themitosan <juliodogers@gmail.com>
2024-10-30 19:53:41 -0300

    +

--- src/main.ts
+++ src/main.ts
@@ -32,13 +32,13 @@ export const
     * Display main logo 
 */
 export function grpp_displayMainLogo(){
-    console.info("\n   <============================================================>");
-    console.info("   <==          Git Repo Preservation Project (GRPP)          ==>");
-    console.info("   <==     Created by Juliana (@julianaheartz.bsky.social)    ==>");
-    console.info("   <============================================================>");
-    console.info("   <==             A classic quote from an old one:           ==>");
-    console.info("   <==                   \"Quem guarda, \x1b[1;32mt\x1b[1;33me\x1b[1;34mm\x1b[0m!\"                  ==>");
-    console.info("   <============================================================>\n");
+    console.info("\n   <=============================================================>");
+    console.info("   <==          Git Repo Preservation Project (GRPP)           ==>");
+    console.info("   <==     Created by Juliana (@julianaheartz.bsky.social)     ==>");
+    console.info("   <=============================================================>");
+    console.info("   <==             A classic quote from an old one:            ==>");
+    console.info("   <==                   \"Quem guarda, \x1b[1;32mt\x1b[1;33me\x1b[1;34mm\x1b[0m!\"                   ==>");
+    console.info("   <=============================================================>\n");
 }
 
 /**
@@ -94,7 +94,7 @@ function startApp(){
 
         // Import repo from list
         if (currentFlag.indexOf('--importList=') !== -1){
-            
+
         }
 
         // Start GRPP update process
@@ -107,9 +107,9 @@ function startApp(){
 
     // If no args were provided
     if (process.argv.length < 3){
-        console.info("   <============================================================>");
+        console.info("   <=============================================================>");
         console.info("     ...Since no args were provided, We wish you a great day! <3");
-        console.info("   <============================================================>\n");
+        console.info("   <=============================================================>\n");
     }
 
 }
