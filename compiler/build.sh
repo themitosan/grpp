650d20966144d7ea0eb42a67c206a6c9faf29b25
themitosan <juliodogers@gmail.com>
2024-11-13 22:48:43 -0300

    Compiler: update build.sh

--- compiler/build.sh
+++ compiler/build.sh
@@ -6,27 +6,25 @@
 
 # Clear screen and display logo
 clear
-echo -e ""
-echo -e "   <=============================================================>"
-echo -e "   <=|          Git Repo Preservation Project (GRPP)           |=>"
-echo -e "   <=|     Created by Juliana (@julianaheartz.bsky.social)     |=>"
-echo -e "   <=============================================================>"
-echo -e "   <=|             A classic quote from an old one:            |=>"
-echo -e "   <=|                   \"Quem guarda, \033[1;32mt\033[1;33me\033[1;34mm\033[0m!\"                   |=>"
-echo -e "   <=============================================================>\n"
+printf "\n   <=============================================================>\n"
+printf "   <=|          Git Repo Preservation Project (GRPP)           |=>\n"
+printf "   <=|     Created by Juliana (@julianaheartz.bsky.social)     |=>\n"
+printf "   <=============================================================>\n"
+printf "   <=|             A classic quote from an old one:            |=>\n"
+printf "   <=|                   \"Quem guarda, \033[1;32mt\033[1;33me\033[1;34mm\033[0m!\"                   |=>\n"
+printf "   <=============================================================>\n\n"
 
 # Build webpack
-echo -e "INFO - Running webpack..."
+echo "INFO - Running webpack..."
 webpack --config ./compiler/build-webpack.config.js
 
 # Appending required node string in order to make it executable
-echo -e "INFO - Finalizing script..."
+echo "INFO - Finalizing script..."
 node compiler/build-finalize.js
 
 # Create bundle file
-echo -e "INFO - Creating bundle file..."
+echo "INFO - Creating bundle file..."
 npm pack
 
 # Display process complete
-echo -e ""
-echo -e "INFO - Process complete!"
+echo "INFO - Process complete!"
